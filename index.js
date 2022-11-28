0b724a2145c9b7f21af1413a7860de83dbc09ea6
Dex1ty <callumthomson263@gmail.com>
2022-11-28 20:36:36 +1100

    234234

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const slashComm = require(filePath);
 if("data" in slashComm && "run" in slashComm) {
-client.slashCommands.set(slashComm.data.name, slashComm);
+client.slashCommands.set(slashComm.data.name.toJSON(), slashComm);
 } else {
   console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "run" property.`);
 }
