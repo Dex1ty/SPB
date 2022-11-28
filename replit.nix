a581c38156d6e96bf3244d8ce93cc23a1bcd0ab0
Dex1ty <callumthomson263@gmail.com>
2022-11-29 10:02:26 +1100

    trewrwer

--- replit.nix
+++ replit.nix
@@ -4,5 +4,7 @@
         pkgs.nodePackages.typescript-language-server
         pkgs.nodePackages.yarn
         pkgs.replitPackages.jest
+        pkgs.libuuid
 	];
+  env = { LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid]; };
 }
